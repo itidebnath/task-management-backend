8bceee71ad0a6d477ab003c51da16e33868fcefe
Iti Debnath <itiidebnath60@gmail.com>
2026-01-17 17:27:15 +0530

    Fix CORS for Vercel

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cors({
   origin: [
       "http://localhost:5173",
-      "https://task-management-frontend-six-smoky.vercel.app/"
+      "https://task-management-frontend-six-smoky.vercel.app"
     ],
   credentials: true
 }))
